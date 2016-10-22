74e7ea843586c795245426c7cb2145e43ad63d96
zhufree <zhufree2013@gmail.com>
2016-10-22 14:04:22 +0800

    增加文档

---
+++ weblate_tools/basic.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+import os
+import re
+
+
+class WeblateBasic():
+
+    def __init__(self):
+        pass
+
+    def set_dir_name(self, dir_name):
+        self.dir_name = dir_name
